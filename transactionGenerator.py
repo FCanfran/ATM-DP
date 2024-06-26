204103a54e5e901d5dfcac82a4022b4ebfcc8d10
Fernando Martin Canfran <ferchomartin9@gmail.com>
2024-06-26 18:16:34 +0200

    moment tx distributor

--- transactionGenerator.py
+++ transactionGenerator.py
@@ -4,6 +4,30 @@ import random
 import datetime
 from geopy.distance import geodesic, great_circle
 
+# NOTE: duration of a transaction will be strictly lower than the TMIN
+# what it is more, it should be insignificant wrt to TMIN so that we do not
+# interfere to the assumptions on the distribution of the transactions on a day...
+def distribute_tx(n):
+    # TODO: pass by param 
+    TMIN = 3600 # assume is 1h 
+    # in seconds of a day: (86400s in a day) -> [tmin/2, 86400-(tmin/2)]
+    lower_bound = TMIN/2
+    upper_bound = 86400 - (TMIN/2)
+
+    if (upper_bound - lower_bound) < (n-1) * TMIN:
+        raise ValueError(f"Impossible to distribute {n} transactions over a day with tmin = {TMIN}")
+
+    moments = []
+    while len(moments) < n:
+        candidate = random.uniform(lower_bound, upper_bound)
+        # to add this new moment of transaction, it is required that it respects 
+        # the time distance constraint wrt all the other added moments
+        if (all(abs(candidate - second) >= TMIN for second in moments)):
+            moments.append(candidate)
+    
+    moments.sort()
+    print(moments)
+
 def calculate_distance(atm_row, point):
     atm_loc = (atm_row['loc_latitude'], atm_row['loc_longitude'])
     distance = great_circle(atm_loc, point).kilometers
@@ -55,7 +79,7 @@ def transaction_generator(card, atm_df, start_date, tx_id):
     np.random.seed(int(key))
 
     # 1. Ordered list of terminals by ascending distance to the client card location
-    # 2. ATMs subset - select a maximum of MAX_SIZE_ATM_SUBSET of ATMs that are at a distance
+    # & ATMs subset - select a maximum of MAX_SIZE_ATM_SUBSET of ATMs that are at a distance
     # inferior or equal to MAX_DISTANCE to the residence of the client
     # TODO: Improve this - values are "testing" values
     MAX_SIZE_ATM_SUBSET = 10
@@ -63,8 +87,15 @@ def transaction_generator(card, atm_df, start_date, tx_id):
     atm_df_ordered = get_ordered_atms(card['loc_latitude'], card['loc_longitude'], atm_df, MAX_SIZE_ATM_SUBSET, MAX_DISTANCE)
 
     print(atm_df_ordered)
+
+    # T_MIN: Minimum threshold time in between 2 transactions of this client
+    # TODO: Calculate T_MIN? - based on the max distance between 2 atms of the subset list
+    # NOTE: Approx -> 2 x MAX_DISTANCE kms is the upper bound on this max distance btw 2 atms of the subset list
+    # Therefore we set the t_min approx to be the time needed to traverse that distance at 50km/h
+    T_MIN = ((MAX_DISTANCE * 2) / 50) * 60 * 60 # in seconds
+    print(T_MIN)
     
-    """
+    # 3. Generation of transactions
     num_days = 10
     for day in range(num_days):
         
@@ -78,33 +109,34 @@ def transaction_generator(card, atm_df, start_date, tx_id):
             for tx in range(num_tx):
                 # transaction_start & transaction_end
                 # 1. transaction_start
+                
+
                 # Time of transaction: Around noon, std 20000 seconds. 
                 # This choice aims at simulating the fact that most transactions occur during the day.
                 # 24h x 60 min x 60 s = 86400s in a day
                 # half day (noon) = 86400s / 2
-                start_time_tx = int(np.random.normal(86400/2, 20000))
-                print(start_time_tx)
+                #start_time_tx = int(np.random.normal(86400/2, 20000))
                 # If transaction time between 0 and 86400, let us keep it, otherwise, let us discard it
-                if (start_time_tx > 0) and (start_time_tx < 86400):
+                #if (start_time_tx > 0) and (start_time_tx < 86400):
                     # shift based on the number of day
-                    start_time_tx = (86400 * day) + start_time_tx 
-                    start_time_delta = datetime.timedelta(seconds=start_time_tx)
+                    #start_time_tx = (86400 * day) + start_time_tx 
+                    #start_time_delta = datetime.timedelta(seconds=start_time_tx)
                     # Add the timedelta to the start date
-                    transaction_start = start_datetime + start_time_delta
-                    print(transaction_start)
+                    #transaction_start = start_datetime + start_time_delta
+                    #print(transaction_start)
 
                     # 2. transaction_end
                     # -> increment some diff time based on the normal duration of a transaction
                     # TODO: Define this better
                     # -> for the moment: mean = 5min (300s), std = 2min (120s)
-                    diff_end = int(np.random.normal(300,120))
-                    if (diff_end < 0): diff_end = 300 # if negative -> then it is = to the mean
+                    #diff_end = int(np.random.normal(300,120))
+                    #if (diff_end < 0): diff_end = 300 # if negative -> then it is = to the mean
 
-                    end_time_tx = start_time_tx + diff_end
-                    end_time_delta = datetime.timedelta(seconds=end_time_tx)
+                    #end_time_tx = start_time_tx + diff_end
+                    #end_time_delta = datetime.timedelta(seconds=end_time_tx)
                     # Add the timedelta to the start date
-                    transaction_end = start_datetime + end_time_delta
-                    print(transaction_end)
+                    #transaction_end = start_datetime + end_time_delta
+                    #print(transaction_end)
 
                     # -----------------------------------------------------------
                     # transaction_amount 
@@ -133,7 +165,6 @@ def transaction_generator(card, atm_df, start_date, tx_id):
                     tx_id += 1 
 
     return transaction_df, tx_id
-    """
 
 def main():
     # Read the card and atm datasets
@@ -156,11 +187,14 @@ def main():
     # TODO: Define the start date
     start_date = "2018-04-01"
     tx_id = 0
+
+    distribute_tx(10)
+    """
     # 1st card
     tx_card, tx_id = transaction_generator(card_df.iloc[0], atm_df, start_date, tx_id)
     print(tx_card)
     print(tx_id) # to continue the next transaction_id on this value
-
+    """
     
 
 
