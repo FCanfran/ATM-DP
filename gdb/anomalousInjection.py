5c177f539eab47df9b71c34d52135e459aeb44b2
Fernando Martin Canfran <ferchomartin9@gmail.com>
2024-09-21 13:19:13 +0200

    tx generation - same random number generator fix

--- gdb/anomalousInjection.py
+++ gdb/anomalousInjection.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import numpy as np
-import random
 import datetime
 from geopy.distance import geodesic, great_circle
 import sys
+from bitarray import bitarray
 
 # Parameters
 # --------------------------------------------------------------------------
@@ -26,6 +26,11 @@ fail_cards = 0
 success_cards = 0
 
 
+# 2 approaches for the distance:
+# - Haversine: (great-circle distance) Earth as a sphere. Less accurate. Less expensive computation.
+# - Vicenty: Earth as a ellipsoid (oblate spheroid). More accurate. More expensive computation.
+# NOTE that: Earth is neither perfectly spherical nor ellipse hence calculating the distance on its surface is a challenging task.
+# https://www.neovasolutions.com/2019/10/04/haversine-vs-vincenty-which-is-the-best/
 def calculate_distance(atm_row, point):
     atm_loc = (atm_row["loc_latitude"], atm_row["loc_longitude"])
     distance = great_circle(atm_loc, point).kilometers
@@ -34,11 +39,6 @@ def calculate_distance(atm_row, point):
 
 # Get ordered ascending list by distance of the atms wrt card location coordinates
 # Optional: limit to the ones that lie inside a specific distance threshold
-# 2 approaches for the distance:
-# - Haversine: (great-circle distance) Earth as a sphere. Less accurate. Less expensive computation.
-# - Vicenty: Earth as a ellipsoid (oblate spheroid). More accurate. More expensive computation.
-# NOTE that: Earth is neither perfectly spherical nor ellipse hence calculating the distance on its surface is a challenging task.
-# https://www.neovasolutions.com/2019/10/04/haversine-vs-vincenty-which-is-the-best/
 def get_ordered_atms(
     card_loc_latitude, card_loc_longitude, atm_df, max_size_subset, max_distance
 ):
@@ -50,20 +50,22 @@ def get_ordered_atms(
         calculate_distance, point=card_loc, axis=1
     )
 
-    # Subset that has distance <= max_distance
-    atm_df_ordered = atm_df_ordered[atm_df_ordered["distance"] <= max_distance]
-
     # Sort DataFrame based on distance
     atm_df_ordered = atm_df_ordered.sort_values(
         by="distance", ascending=True
     ).reset_index(drop=True)
 
+    # The "regular" subset: select those with distance <= max_distance
+    atm_df_regular = atm_df_ordered[atm_df_ordered["distance"] <= max_distance]
+    # The "non-regular" subset: the rest
+    atm_df_non_regular = atm_df_ordered[atm_df_ordered["distance"] > max_distance]
+
     # Subset of max size of max_size_subset
-    atm_df_ordered = atm_df_ordered.head(max_size_subset)
+    atm_df_regular = atm_df_regular.head(max_size_subset)
 
     # TODO: Give priority to the ATMs belonging to the same bank company as the card
 
-    return atm_df_ordered
+    return atm_df_regular, atm_df_non_regular
 
 
 # Distribute n transactions on a day [tmin/2, 86400-(tmin/2)]
@@ -81,7 +83,7 @@ def distribute_tx(n, t_min):
 
     moments = []
     while len(moments) < n:
-        candidate = int(random.uniform(lower_bound, upper_bound))
+        candidate = int(np.random.uniform(lower_bound, upper_bound))
         # to add this new moment of transaction, it is required that it respects
         # the time distance constraint wrt all the other added moments
         if all(abs(candidate - second) >= (t_min + max_duration) for second in moments):
@@ -110,7 +112,7 @@ def transaction_generator(card, atm_df, start_date, tx_id):
     # 1. Ordered list of terminals by ascending distance to the client card location
     # selecting a maximum of max_size_atm_subset of ATMs that are at a distance
     # inferior or equal to max_distance to the residence of the client
-    atm_df_ordered = get_ordered_atms(
+    atm_df_regular, atm_df_non_regular = get_ordered_atms(
         card["loc_latitude"],
         card["loc_longitude"],
         atm_df,
@@ -118,7 +120,7 @@ def transaction_generator(card, atm_df, start_date, tx_id):
         max_distance,
     )
 
-    if len(atm_df_ordered) > 0:
+    if len(atm_df_regular) > 0:
         # T_MIN: Minimum threshold time in between 2 transactions of this client
         # TODO: Calculate t_min? - based on the max distance between 2 atms of the subset list
         # NOTE: Approx -> 2 x MAX_DISTANCE kms is the upper bound on this max distance btw 2 atms of the subset list
@@ -137,8 +139,8 @@ def transaction_generator(card, atm_df, start_date, tx_id):
                     # 0. ATM id
                     # randomly among the subset of ATMs -> all of them satisfy the constraints
                     # of the min threshold time TMIN etc...
-                    rand_index = random.choice(atm_df_ordered.index)
-                    ATM_id = atm_df_ordered.loc[rand_index]["ATM_id"]
+                    rand_index = np.random.choice(atm_df_regular.index)
+                    ATM_id = atm_df_regular.loc[rand_index]["ATM_id"]
                     # 1. transaction_start
                     # shift based on the number of day
                     start_time_tx = (86400 * day) + moment
@@ -201,17 +203,36 @@ def transaction_generator(card, atm_df, start_date, tx_id):
         global success_cards
         success_cards += 1
 
-    return transaction_df, tx_id
+    return transaction_df, tx_id, atm_df_non_regular
 
 
 # Introduction of anomalous tx to cause the fraud pattern 1
-def introduce_anomalous_fp_1(regular_tx_card, ratio):
-    print(regular_tx_card)
+def introduce_anomalous_fp_1(regular_tx_card, ratio, atm_non_regular):
     num_regular = len(regular_tx_card)
     num_anomalous = round(num_regular * ratio)
     print(num_regular, num_anomalous)
 
     # randomly select in between which tx the anomalous are introduced
+
+    # bit array to mark occupied and free tx "holes" - python bitarray
+    # - holes indicate the position i in between which tx the anomalous tx is to be inserted:
+    # i in [0,num_regular-1]
+    # - i = 2 -> indicates that the anomalous tx is to be inserted in between the tx 2 and 3
+    # after the tx 2
+    holes = bitarray(num_regular)
+    holes.setall(0)
+    anomalous = 0
+    # :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: #
+    while anomalous < num_anomalous:
+        # random hole selection in [0, num_regular-1]
+        index = np.random.randint(0, num_regular)
+        if holes[index] == 0:
+            # not occupied, mark as occupied
+            holes[index] = 1
+            print(index)
+            # introduce anomalous tx in this position
+            anomalous += 1
+    # :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: #
 
 
 def main():
@@ -224,16 +245,17 @@ def main():
 
     output_file_name = sys.argv[1]
     try:
-        num_anomalous = float(sys.argv[2])
+        anomalous_ratio = float(sys.argv[2])
+        if not 0 <= anomalous_ratio <= 1:
+            raise ValueError
     except ValueError:
         print(
-            "Error: The <m: ratio of anomalous tx (per card)> argument must be a float."
+            "Error: The <m: ratio of anomalous tx (per card)> argument must be a float in [0,1]."
         )
         sys.exit(1)
 
     # fix a constant seed so that experiments are reproducible
     key = 37
-    random.seed(int(key))
     np.random.seed(int(key))
 
     # Read the card and atm datasets
@@ -256,19 +278,23 @@ def main():
     tx_id = 0
 
     for card_index in card_df.index:
-        tx_card, tx_id = transaction_generator(
+        tx_card, tx_id, atm_non_regular = transaction_generator(
             card_df.iloc[card_index], atm_df, start_date, tx_id
         )
-        # Introduction of anomalous tx
-        introduce_anomalous_fp_1(tx_card, num_anomalous)
-        print(
-            "########################################################################################################################################"
-        )
+        if len(tx_card) > 0:
+            # Introduction of anomalous tx
+            introduce_anomalous_fp_1(tx_card, anomalous_ratio, atm_non_regular)
+            print(
+                "########################################################################################################################################"
+            )
+
         # if transaction_df is empty (on first iteration) then directly assign the returned df, otherwise an ordinary concat
+        # Drop all-NaN rows from tx_card before concatenation
+        tx_card_cleaned = tx_card.dropna(how="all").dropna(axis=1, how="all")
         transaction_df = (
-            tx_card.copy()
+            tx_card_cleaned.copy()
             if transaction_df.empty
-            else pd.concat([transaction_df, tx_card], ignore_index=True)
+            else pd.concat([transaction_df, tx_card_cleaned], ignore_index=True)
         )
 
     # print(transaction_df)
@@ -286,13 +312,13 @@ def main():
     print("\n")
     print("~~~~~~~~~~~~~~~~~~~ Summary ~~~~~~~~~~~~~~~~~~~~~")
     print(
-        f"Number of transactions created:                                     {tx_id}"
+        f"Number of transactions created:                                       {tx_id}"
     )
     print(
-        f"Number of Cards with success:                                       {success_cards}"
+        f"Number of Cards with success:                                         {success_cards}"
     )
     print(
-        f"Number of Cards with failure (no transactions could be generated):  {fail_cards}"
+        f'Number of Cards with "failure" (no transactions could be generated):  {fail_cards}'
     )
     print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 
