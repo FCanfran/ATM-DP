a45d112f2cb0b4b37e4338c0915507480d062155
Fernando Martin Canfran <ferchomartin9@gmail.com>
2024-11-23 19:55:55 +0100

    time scaler

--- gdb/timeScaler.py
+++ gdb/timeScaler.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import sys
-from datetime import datetime
+from datetime import datetime, timedelta
 
 # Pre:
 # - input stream comes ordered by timestamp
@@ -17,25 +17,63 @@ def main():
     # read stream input file to be scaled
     tx_df = pd.read_csv(tx_file)
 
+    # convert the timestamp fields to datetime type
+    tx_df["transaction_start"] = pd.to_datetime(
+        tx_df["transaction_start"], errors="coerce"
+    )
+    tx_df["transaction_end"] = pd.to_datetime(tx_df["transaction_end"], errors="coerce")
+
     # calculate the t: size of time interval of the given file
-    interval_start = tx_df.iloc[0]["transaction_start"]  # first tx
-    interval_end = tx_df.iloc[-1]["transaction_end"]  # last tx
-    # convert to datetime
-    dt1 = datetime.strptime(interval_start, format)
-    dt2 = datetime.strptime(interval_end, format)
-    # Calculate the difference in seconds - size of the original time interval t
-    t_original = int((dt2 - dt1).total_seconds())
-    print(f"The original time interval size is t = {t_original} s")
+    first_timestamp = tx_df.iloc[0]["transaction_start"]  # first tx
+    last_timestamp = tx_df.iloc[-1]["transaction_end"]  # last tx
+    # Calculate the difference in seconds - size of the original time interval T
+    T_original = int((last_timestamp - first_timestamp).total_seconds())
+    print(f"The original time interval size is T = {T_original} s")
 
     validInt = False
     while not validInt:  # Loop until a valid integer is entered
         try:
-            t_prime = int(
-                input("Introduce the t' (in seconds) to scale the input stream: ")
+            T_new = int(
+                input("Introduce the T' (in seconds) to scale the input stream: ")
             )
             validInt = True
         except ValueError:
-            print("Invalid input. Please enter a valid integer as t'")
+            print("Invalid input. Please enter a valid integer as T'")
+
+    # scaling timestamps
+    # t' = t0 + ((t' - t0) x scale_factor)
+    scale_factor = T_new / T_original
+    print(scale_factor)
+
+    tx_df["transaction_start_scaled"] = tx_df["transaction_start"].apply(
+        lambda t_start: first_timestamp
+        + timedelta(
+            seconds=((t_start - first_timestamp).total_seconds() * scale_factor)
+        )
+    )
+
+    # only if transaction_end is not NaN
+    tx_df["transaction_end_scaled"] = tx_df["transaction_end"].apply(
+        lambda t_end: (
+            first_timestamp
+            + timedelta(
+                seconds=((t_end - first_timestamp).total_seconds() * scale_factor)
+            )
+            if pd.notna(t_end)
+            else pd.NaT
+        )  # Handle NaN (missing values)
+    )
+
+    tx_df["transaction_start_scaled"] = tx_df["transaction_start_scaled"].dt.strftime(
+        format
+    )
+    tx_df["transaction_end_scaled"] = tx_df["transaction_end_scaled"].dt.strftime(
+        format
+    )
+
+    outfilename = tx_file.replace(".csv", "-scaled.csv")
+    tx_df.to_csv(outfilename, index=False)
+    print(f"Scaled stream file saved to {outfilename}")
 
 
 if __name__ == "__main__":
