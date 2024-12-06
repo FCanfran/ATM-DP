80089b458629db4249a05e16a9345e12daa294e4
Fernando Martin Canfran <ferchomartin9@gmail.com>
2024-12-06 17:11:17 +0100

    exps cluster

---
+++ engine-no-real-time/experiments-cluster/exps-scripts/small/4c/4c-2f.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -l
+#
+#SBATCH -J exp-4c-2f
+#SBATCH -o exp-4c-2f."%j".out
+#SBATCH -e exp-4c-2f."%j".err
+#
+#SBATCH --mail-user fernando.martin.canfran@estudiantat.upc.edu
+#SBATCH --mail-type=ALL
+#
+#SBATCH --mem=1024M
+#SBATCH -c 4
+#SBATCH -p short
+
+../cmd/main exps-descriptions/small/4c/4c-2f.csv
